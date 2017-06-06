a8fc9f2d8e0c6bb4ab48b764167dc6f8d3055bc1
Gustavo Bezerra <gusutabopb@gmail.com>
2017-06-06 18:15:21 +0900

    Debug make_line - Escaping issues - Empty tags bug

--- aioinflux/line_protocol.py
+++ aioinflux/line_protocol.py
@@ -1,7 +1,12 @@
-from typing import Iterable, Mapping, AnyStr
+from typing import Iterable, Mapping
 
 import pandas as pd
 
+escapes = str.maketrans({'\\': r'\\', '"': r'\"',
+                         '\n': r'\n', "'": r"\'",
+                         ' ': r'\ ',  ',': r'\,',
+                         '=': r'\=',
+                         })
 
 def parse_data(data):
     if isinstance(data, bytes):
@@ -17,12 +22,16 @@ def parse_data(data):
 
 
 def make_line(point):
-    l = '{measurement},{tags} {fields} {timestamp}'
-    l = l.format(measurement=_parse_measurement(point),
-                 tags=_parse_tags(point),
-                 fields=_parse_fields(point),
-                 timestamp=_parse_timestamp(point))
-    return l.encode('utf-8')
+    p = dict(measurement=_parse_measurement(point),
+             tags=_parse_tags(point),
+             fields=_parse_fields(point),
+             timestamp=_parse_timestamp(point))
+    if point['tags']:
+        line = '{measurement},{tags} {fields} {timestamp}'.format(**p)
+    else:
+        p.pop('tags')
+        line = '{measurement} {fields} {timestamp}'.format(**p)
+    return line.encode('utf-8')
 
 
 def _parse_measurement(point):
@@ -30,7 +39,15 @@ def _parse_measurement(point):
 
 
 def _parse_tags(point):
-    return ','.join(['{k}={v}'.format(k=k, v=v) for k, v in point['tags'].items()])
+    output = []
+    for k, v in point['tags'].items():
+        k = k.translate(escapes)
+        v = v.translate(escapes)
+        output.append('{k}={v}'.format(k=k, v=v))
+    if output:
+        return ','.join(output)
+    else:
+        return ''
 
 
 def _parse_timestamp(point):
@@ -43,8 +60,13 @@ def _parse_timestamp(point):
 def _parse_fields(point):
     output = []
     for k, v in point['fields'].items():
+        k = k.translate(escapes)
         if isinstance(v, int):
             output.append('{k}={v}i'.format(k=k, v=v))
+        elif isinstance(v, bool):
+            output.append('{k}={v}'.format(k=k, v=str(v).upper()))
+        elif isinstance(v, str):
+            output.append('{k}="{v}"'.format(k=k, v=v.translate(escapes)))
         else:
-            output.append('{k}={v}'.format(k=v, v=v))
+            output.append('{k}={v}'.format(k=k, v=v))
     return ','.join(output)
