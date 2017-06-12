c776d0a65a3868c7fc5f57e749a698d2d380f6ea
Gustavo Bezerra <gusutabopb@gmail.com>
2017-06-12 20:04:04 +0900

    Implement simple unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from aioinflux import AsyncInfluxDBClient
 
 @pytest.fixture(scope='module')
 def client():
-    with AsyncInfluxDBClient as client:
+    with AsyncInfluxDBClient(database='mytestdb', sync=True) as client:
         yield client
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,46 @@
+import random
+import datetime
+
+
+def random_point():
+    point = {'measurement': 'test_measurement',  # '"measurement with quo⚡️es and emoji"',
+             'tags': {'tag key with sp🚀ces': 'tag,value,with"commas"'},
+             'time': datetime.datetime.now(),
+             'fields': {
+                 'fi\neld_k\ey': random.randint(0, 200),
+                 'quote': '"'
+                }
+             }
+    return point
+
+
+def random_points(n=10):
+    for i in range(n):
+        yield random_point()
+
+
+def test_ping(client):
+    assert len(client.ping()) == 4
+
+
 def test_create_database(client):
-    pass
-
-
-def test_simple_select_query(client):
-    pass
+    resp = client.create_database(db='mytestdb')
+    assert resp['resp'].status == 200
 
 
 def test_simple_write(client):
-    pass
+    print(client.db)
+    assert client.write(random_points(10))
+
+
+def test_simple_query(client):
+    resp = client.select_all(measurement='test_measurement')
+    assert len(resp['json']['results'][0]['series'][0]['values']) == 10
+
+
+def test_drop_measurement(client):
+    client.drop_measurement(measurement='test_measurement')
+
+
+def test_drop_database(client):
+    client.drop_database(db='mytestdb')
