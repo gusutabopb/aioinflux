61d47f4b5849b5e6bc11fac0133d67c323348658
Gustavo Bezerra <gusutabopb@gmail.com>
2017-06-16 19:59:37 +0900

    Rename attributes to make client more user-friendly - Make many attributes/methods private

--- aioinflux/client.py
+++ aioinflux/client.py
@@ -19,11 +19,11 @@ def runner(coro):
 
     @wraps(coro)
     def inner(self, *args, **kwargs):
-        if not self.sync:
+        if self.async:
             return coro(self, *args, **kwargs)
-        resp = self.loop.run_until_complete(coro(self, *args, **kwargs))
+        resp = self._loop.run_until_complete(coro(self, *args, **kwargs))
         if self.dataframe and coro.__name__ == 'query':
-            return self.make_df(resp)
+            return self._make_df(resp)
         else:
             return resp
 
@@ -37,7 +37,7 @@ class AsyncInfluxDBClient:
     def __init__(self, host: str = 'localhost', port: int = 8086,
                  username: Optional[str] = None, password: Optional[str] = None,
                  database: str = 'testdb', loop: asyncio.BaseEventLoop = None,
-                 log_level: int = 30, dataframe: bool = False, sync: bool = False):
+                 log_level: int = 30, dataframe: bool = False, async: bool = True):
         """
         The AsyncInfluxDBClient object holds information necessary to interect with InfluxDB.
         It is async by default, but can also be used as a sync/blocking client and even generate
@@ -58,55 +58,57 @@ class AsyncInfluxDBClient:
         :param log_level: Logging level. The lower the more verbose. Defaults to INFO (30).
         :param dataframe: Setting to `True` make query results be parsed into a Pandas DataFrame.
                           When set to `False` (default), query results will be returned as dictionaries.
-        :param sync: Setting to `True` will make the client behave in a sychronous way.
-                     Setting `self.dataframe` to True automatically sets `sync` to True as well.
+        :param async: Setting to `False` will make the client behave in a sychronous/blocking way.
+                      Setting `self.dataframe` to True automatically sets `async` to False.
         """
-        self.logger = self._make_logger(log_level)
-        self.loop = asyncio.get_event_loop() if loop is None else loop
-        self.auth = aiohttp.BasicAuth(username, password) if username and password else None
-        self.session = aiohttp.ClientSession(loop=self.loop, auth=self.auth)
+        self._logger = self._make_logger(log_level)
+        self._loop = asyncio.get_event_loop() if loop is None else loop
+        self._auth = aiohttp.BasicAuth(username, password) if username and password else None
+        self._session = aiohttp.ClientSession(loop=self._loop, auth=self._auth)
+        self._url = f'http://{host}:{port}/{{endpoint}}'
         self.db = database
         self.url = f'http://{host}:{port}/{{endpoint}}'
         self.dataframe = dataframe
-        self.sync = sync or dataframe
-        if dataframe and not sync:
-            self.logger.warning('Setting to `dataframe` to `True` makes client run in sync mode.')
+        self.async = async and not dataframe
+        if async and dataframe:
+            self._logger.warning('Setting to `dataframe` to `True` makes client run in sync/blocking mode.')
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.session.close()
+        self._session.close()
 
     def __del__(self):
-        self.session.close()
+        self._session.close()
 
     @runner
     async def ping(self) -> dict:
-        """Pings InfluxDB. Returns a dictionary containing the headers of the response from `influxd`."""
-        async with self.session.get(self.url.format(endpoint='ping')) as resp:
-            self.logger.info(f'{resp.status}: {resp.reason}')
+        """Pings InfluxDB.
+         Returns a dictionary containing the headers of the response from `influxd`.
+         """
+        async with self._session.get(self._url.format(endpoint='ping')) as resp:
+            self._logger.info(f'{resp.status}: {resp.reason}')
             return dict(resp.headers.items())
 
     @runner
     async def write(self, data: Union[PointType, Iterable[PointType]]) -> bool:
-        """
-        Writes query to InfluxDB.
+        """Writes data to InfluxDB.
         Input can be:
         1) a string properly formatted in InfluxDB's line protocol
         2) a dictionary containing four items ('measurement', 'time', 'tags', 'fields')
         3) a Pandas DataFrame with a DatetimeIndex
         4) an iterable of one of above
-        Input data in the 2-4 formats are parsed to the line protocol before being written to InfluxDB.
+        Input data in formats 2-4 are parsed to the line protocol before being written to InfluxDB.
         See also: https://docs.influxdata.com/influxdb/v1.2/write_protocols/line_protocol_reference/
 
         :param data: Input data (see description above).
         :return: Returns `True` if insert is sucessfull. Raises `ValueError` exception otherwise.
         """
         data = parse_data(data)
-        self.logger.debug(data)
-        url = self.url.format(endpoint='write') + '?' + urlencode(dict(db=self.db))
-        async with self.session.post(url, data=data) as resp:
+        self._logger.debug(data)
+        url = self._url.format(endpoint='write') + '?' + urlencode(dict(db=self.db))
+        async with self._session.post(url, data=data) as resp:
             if resp.status == 204:
                 return True
             else:
@@ -116,8 +118,7 @@ class AsyncInfluxDBClient:
     @runner
     async def query(self, q: AnyStr, db=None, epoch='ns',
                     chunked=False, chunk_size=None, **kwargs) -> Union[AsyncGenerator, dict]:
-        """
-        Sends a query to InfluxDB.
+        """Sends a query to InfluxDB.
         Please refer to the InfluxDB documentation for all the possible queries:
         https://docs.influxdata.com/influxdb/v1.2/query_language/spec/#queries
 
@@ -125,8 +126,8 @@ class AsyncInfluxDBClient:
         :param db: Database parameter. Defaults to `self.db`
         :param epoch: Precision level of response timestamps.
             Valid values: ``{'ns', 'u', 'µ', 'ms', 's', 'm', 'h'}``.
-        :param chunked: Retrieves the points in streamed batches instead of in a single response and returns
-            and AsyncGenerator which will yield point by point.
+        :param chunked: Retrieves the points in streamed batches instead of in a single
+            response and returns an AsyncGenerator which will yield point by point.
         :param chunk_size: Max number of points for each chunk.
             InfluxDB chunks responses by series or by every 10,000 points, whichever occurs first.
         :param kwargs: String interpolation arguments for partialmethods
@@ -156,20 +157,20 @@ class AsyncInfluxDBClient:
         else:
             method = 'post'
 
-        url = self.url.format(endpoint='query')
-        func = getattr(self.session, method)
+        url = self._url.format(endpoint='query')
+        func = getattr(self._session, method)
         data = dict(params=data) if method == 'get' else dict(data=data)
-        self.logger.debug(data)
+        self._logger.debug(data)
 
         if chunked:
             return _chunked_generator(func, url, data)
 
         async with func(url, **data) as resp:
-            self.logger.info(f'{resp.status}: {resp.reason}')
+            self._logger.info(f'{resp.status}: {resp.reason}')
             output = await resp.json()
-            self.logger.debug(resp)
-            self.logger.debug(output)
-            self.check_error(output)
+            self._logger.debug(resp)
+            self._logger.debug(output)
+            self._check_error(output)
             return output
 
     # Below are methods that wrap ``AsyncInfluxDBClient.query`` in order to provide
@@ -201,7 +202,7 @@ class AsyncInfluxDBClient:
         return logger
 
     @staticmethod
-    def make_df(resp):
+    def _make_df(resp):
         """Makes list of DataFrames from a response object"""
         def _make_df(series):
             df = pd.DataFrame(series['values'], columns=series['columns'])
@@ -217,7 +218,7 @@ class AsyncInfluxDBClient:
                 for series in statement['series']]
 
     @staticmethod
-    def check_error(response):
+    def _check_error(response):
         if 'error' in response:
             raise InfluxDBError(response['error'])
         elif 'results' in response:
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from aioinflux import AsyncInfluxDBClient
 
 @pytest.fixture(scope='module')
 def client():
-    with AsyncInfluxDBClient(database='mytestdb', sync=True, log_level=5) as client:
+    with AsyncInfluxDBClient(database='mytestdb', async=False, log_level=5) as client:
         yield client
