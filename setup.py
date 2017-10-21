c73f91c07ab92bef0439862c3cd0dde625c0b4d2
Gustavo Bezerra <gusutabopb@gmail.com>
2017-10-21 11:10:50 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(name='aioinflux',
-      version='0.1.0',
+      version='0.1.1-dev1',
       description='Asynchronous Python client for InfluxDB',
       long_description=long_description,
       author='Pluga AI',
