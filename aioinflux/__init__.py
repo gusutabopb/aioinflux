4f5d8a223baaeb0af0afbf4a2aa646b8b4af3f3f
Gustavo Bezerra <gusutabopb@gmail.com>
2018-09-07 13:13:54 +0900

    Fix import

--- aioinflux/__init__.py
+++ aioinflux/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+from . import serialization
 from .client import InfluxDBClient, InfluxDBError, InfluxDBWriteError, logger
 from .iterutils import iterpoints, InfluxDBResult, InfluxDBChunkedResult
 
