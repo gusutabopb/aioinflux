e948e17e96befc37e48d8eb0805982d4dd7a137d
Gustavo Bezerra <gusutabopb@gmail.com>
2018-09-13 22:52:24 +0900

    Expose `datapoint` and `InfluxType` at package top level

--- aioinflux/__init__.py
+++ aioinflux/__init__.py
@@ -2,5 +2,6 @@
 from . import serialization
 from .client import InfluxDBClient, InfluxDBError, InfluxDBWriteError, logger
 from .iterutils import iterpoints, InfluxDBResult, InfluxDBChunkedResult
+from .serialization.datapoint import datapoint, InfluxType
 
 __version__ = '0.4.0dev0'
