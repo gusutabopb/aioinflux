d97b16c1ef24b126d77337e984f941de66f69a0f
Gustavo Bezerra <gusutabopb@gmail.com>
2019-01-16 13:56:47 +0900

    Minor refactoring

--- aioinflux/serialization/usertype.py
+++ aioinflux/serialization/usertype.py
@@ -30,20 +30,8 @@ FLOAT = TypeVar('FLOAT', bound=float)
 STR = TypeVar('STR', bound=str)
 ENUM = TypeVar('ENUM', enum.Enum, str)
 
-influx_types = {
-    MEASUREMENT: 0,
-    TIMEINT: 10,
-    TIMESTR: 11,
-    TIMEDT: 12,
-    TAG: 20,
-    TAGENUM: 21,
-    # Fields (>25)
-    BOOL: 30,
-    INT: 40,
-    FLOAT: 50,
-    STR: 60,
-    ENUM: 61,
-}
+time_types = [TIMEINT, TIMEDT, TIMESTR]
+field_types = [BOOL, INT, FLOAT, STR, ENUM]
 
 
 class SchemaError(TypeError):
@@ -64,8 +52,21 @@ def dt_to_int(dt):
     return int(dt.timestamp()) * 10 ** 9 + dt.microsecond * 1000
 
 
+def _validate_schema(schema, placeholder):
+    c = Counter(schema.values())
+    if not c:
+        raise SchemaError("Schema/type annotations missing")
+    if c[MEASUREMENT] > 1:
+        raise SchemaError("Class can't have more than one 'MEASUREMENT' attribute")
+    if sum(c[e] for e in time_types) > 1:
+        raise SchemaError(f"Can't have more than one timestamp-type attribute {time_types}")
+    if sum(c[e] for e in field_types) < 1 and not placeholder:
+        raise SchemaError(f"Must have one or more field-type attributes {field_types}")
+
+
 def _make_serializer(meas, schema, rm_none, extra_tags, placeholder):
     """Factory of line protocol parsers"""
+    _validate_schema(schema, placeholder)
     tags = []
     fields = []
     ts = None
@@ -122,42 +123,15 @@ def _make_serializer(meas, schema, rm_none, extra_tags, placeholder):
 def lineprotocol(cls=None, *, schema=None, rm_none=False, extra_tags=None, placeholder=False):
     """Adds to_lineprotocol method to arbitrary user-defined classes
 
-    Can be used as a decorator or as a regular function
-    (for namedtuples generated functionally).
-
-    Main characteristics:
-
-      - Built-in serialization to InfluxDB line protocol through the ``to_lineprotocol`` method.
-      - About 2-3x faster serialization than the ``serialization.mapping`` module.
-
-          - Difference gets smaller (1x-1.5x) when ``rm_none=True`` or when the number of
-            fields/tags is very large (20+).
-
-    :param schema: Dictionary-based (functional namedtuple style)
-        or @dataclass decorator-based (dataclass style) measurement schema
-    :param rm_none: Whether apply a regex to remove ``None`` values from.
+    :param schema: Schema dictionary (attr/type pairs).
+    :param rm_none: Whether apply a regex to remove ``None`` values.
         If ``False``, passing ``None`` values to boolean, integer or float or time fields
         will result in write errors. Setting to ``True`` is "safer" but impacts performance.
     :param extra_tags: Hard coded tags to be added to every point generated.
     """
 
     def _lineprotocol(cls):
-        # Schema validation
-        try:
-            _schema = schema or cls.__annotations__
-        except AttributeError:
-            raise SchemaError("Schema/type annotations missing")
-
-        c = Counter(_schema.values())
-        if not c[MEASUREMENT] <= 1:
-            raise SchemaError("Class can't have more than one 'MEASUREMENT' attribute")
-        if sum([c[e] for e, v in influx_types.items() if 0 < v < 20]) > 1:
-            raise SchemaError("Class can't have more than one timestamp-type attribute "
-                              "('TIMEINT', 'TIMEDT', 'TIMESTR')")
-        if sum([c[e] for e, v in influx_types.items() if v >= 25]) < 1 and not placeholder:
-            raise SchemaError("Class must have one or more field-type attributes "
-                              "('BOOL', 'INT', 'FLOAT', 'ENUM').")
-
+        _schema = schema or getattr(cls, '__annotations__', {})
         args = (_schema, rm_none, extra_tags or {}, placeholder)
         cls.to_lineprotocol = _make_serializer(cls.__name__, *args)
         cls._lineprotocol = args
