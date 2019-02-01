fc66cafd57b3369db5c38c25478367ccaf24e248
Gustavo Bezerra <gusutabopb@gmail.com>
2019-02-01 12:11:49 +0900

    Bump version

--- aioinflux/__init__.py
+++ aioinflux/__init__.py
@@ -4,4 +4,4 @@ from .client import InfluxDBClient, InfluxDBError, InfluxDBWriteError, logger
 from .iterutils import iterpoints
 from .serialization.usertype import *
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
