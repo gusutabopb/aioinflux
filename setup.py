8f75585cfe62547f386fc7a3096a2c8b674b11e7
Gustavo Bezerra <gusutabopb@gmail.com>
2020-12-14 17:52:12 +0900

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -60,5 +60,6 @@ setup(name='aioinflux',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Database',
       ])
